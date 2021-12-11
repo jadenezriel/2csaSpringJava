453a11b6211f8c7460299217f73f50f5eecce6e6
jadenezriel <jadenezriel.go.iics@ust.edu.ph>
2021-12-11 17:43:50 +0800

    Update EmployeeController.java

--- src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controllers/EmployeeController.java
@@ -5,6 +5,11 @@ import com.csa.samplefullstack.repository.EmployeeRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +21,24 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
     
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
+    
+    @PostMapping("/employees")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeFromId(@PathVariable long id){
+        return employeeRepository.findById(id).get();
+    }
+    
+    @PutMapping("/employees")
+    public Employee putEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
 }
